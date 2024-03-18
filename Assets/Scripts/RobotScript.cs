da237bde4060e15461565bc2c0ffd8d6d6f7607d
Niko <niko.laakso@tuni.fi>
2024-03-18 11:08:53 +0200

    ???

--- Assets/Scripts/RobotScript.cs
+++ Assets/Scripts/RobotScript.cs
@@ -7,10 +7,12 @@ namespace pilleripeli
     public class RobotScript : MonoBehaviour
     {
         [SerializeField]
-        private GameObject[] patrolNodes; 
+        private GameObject[] patrolNodes;
         [SerializeField]
         private float baseMovementSpeed;
         private float timeMultiplier = 1.0f;
+        private const string DirectionXAnimationParameter = "DirectionX";
+        private const string DirectionYAnimationParameter = "DirectionY";
         private Vector2 currentTarget;
         private int currentIndex;
         private Transform transform;
@@ -33,6 +35,8 @@ namespace pilleripeli
         void FixedUpdate()
         {
             GetComponent<Transform>().position = Vector3.MoveTowards(transform.position, currentTarget, baseMovementSpeed * timeMultiplier * Time.deltaTime);
+
+
         }
         /*
         void OnTriggerEnter2D(Collider2D col)
