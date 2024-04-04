331efb340c3334bfca3a2c7d0172608f8e0e2dcf
Skunbylgo <kristian.toivonen@tuni.fi>
2024-04-04 07:25:08 +0300

    Add CoffeeTableBehaviour script

---
+++ Assets/Scripts/CoffeeTableBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using pilleripeli;
+using UnityEngine;
+
+namespace Mobiiliesimerkki
+{
+    public class CoffeeTableBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject gameManagerObject;
+        GameManager gameManager;
+
+        void Start()
+        {
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
+        // Start is called before the first frame update
+        public void OnTriggerEnter2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+            {
+                gameManager.LoadCoffee();
+            }
+        }
+        public void OnTriggerExit2D(Collider2D col)
+        {
+            if(col.CompareTag("Player"))
+            {
+                gameManager.UnloadCoffee();
+            }
+        }
+    }
+}
