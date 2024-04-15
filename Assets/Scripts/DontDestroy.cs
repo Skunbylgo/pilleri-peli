b760ca0a6768c988a8ff763cd42d69bb50b6a44a
Skunbylgo <kristian.toivonen@tuni.fi>
2024-04-15 10:50:36 +0300

    Add DontDestroy script

---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pilleripeli
+{
+    public class DontDestroy : MonoBehaviour
+    {
+        void Awake()
+        {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+
+            if (objs.Length > 1)
+            {
+                Destroy(this.gameObject);
+            }
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
