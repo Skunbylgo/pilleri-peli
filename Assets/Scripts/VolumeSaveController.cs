fe47736813c7ae8765c4aa3d4fa932c0c99e5902
Skunbylgo <kristian.toivonen@tuni.fi>
2024-04-19 17:28:19 +0300

    Add music and sfx slider to settings

--- Assets/Scripts/VolumeSaveController.cs
+++ Assets/Scripts/VolumeSaveController.cs
@@ -4,10 +4,16 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Mobiiliesimerkki
+namespace pilleripeli
 {
     public class VolumeSaveController : MonoBehaviour
     {
+        [SerializeField] private Slider effectVolumeSlider = null;
+        [SerializeField] private Text effectVolumeTextUI = null;
+
+        [SerializeField] private Slider musicVolumeSlider = null;
+        [SerializeField] private Text musicVolumeTextUI = null;
+
         [SerializeField] private Slider volumeSlider = null;
         [SerializeField] private Text volumeTextUI = null;
 
@@ -18,20 +24,39 @@ namespace Mobiiliesimerkki
 
         public void VolumeSlider(float volume)
         {
-            volumeTextUI.text = volume.ToString("0.0");
+            volumeTextUI.text = (volume * 100.0f).ToString("000");
+        }
+
+        public void EffectVolumeSlider(float volume)
+        {
+            effectVolumeTextUI.text = (volume * 100.0f).ToString("000");
+        }
+
+        public void MusicVolumeSlider(float volume)
+        {
+            musicVolumeTextUI.text = (volume * 100.0f).ToString("000");
         }
 
         public void SaveVolumeButton()
         {
             float volumeValue = volumeSlider.value;
+            float effectVolumeValue = effectVolumeSlider.value;
+            float musicVolumeValue = musicVolumeSlider.value;
             PlayerPrefs.SetFloat("Volume", volumeValue);
+            PlayerPrefs.SetFloat("EffectVolume", effectVolumeValue);
+            PlayerPrefs.SetFloat("MusicVolume", musicVolumeValue);
             LoadValues();
+            GameObject.Find("MusicManager").GetComponent<MusicManager>().ChangeVolume(musicVolumeValue);
         }
 
         void LoadValues()
         {
             float volumeValue = PlayerPrefs.GetFloat("Volume");
+            float effectVolumeValue = PlayerPrefs.GetFloat("EffectVolume");
+            float musicVolumeValue = PlayerPrefs.GetFloat("MusicVolume");
             volumeSlider.value = volumeValue;
+            effectVolumeSlider.value = effectVolumeValue;
+            musicVolumeSlider.value = musicVolumeValue;
             AudioListener.volume = volumeValue;
         }
     }
